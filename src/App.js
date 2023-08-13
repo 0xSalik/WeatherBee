aeddd1b9744aaf2b3fdd1593338ddfa5dc99459f
Salik Khan <contact@salikkhan.com>
2023-08-13 12:53:12 +0530

    Add icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import WeatherCard from "./components/WeatherCard";
 function App() {
   return (
-    <div className="App">
-      <WeatherCard />
-    </div>
+    <>
+      <div className="Header">
+        WeatherBee &nbsp;
+        <img
+          alt="bee"
+          src="https://uploads-ssl.webflow.com/5f778340ed26b167bd087abe/63fccc46c5784ae167b90edc_buzzing%20bee.svg"
+          width={50}
+        />
+      </div>
+      <div className="App">
+        <WeatherCard />
+      </div>
+    </>
   );
 }
 
